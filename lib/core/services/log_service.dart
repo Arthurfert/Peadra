d8b673cec6b1c8d0f93ef18e846bab053b577ae5
Arthurfert <fert.arthur83@gmail.com>
2026-07-23 16:58:55 +0200

    feat: logs shows app errors and warnings

--- lib/core/services/log_service.dart
+++ lib/core/services/log_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:intl/intl.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
@@ -8,9 +9,21 @@ class LogService {
   factory LogService() => _instance;
   LogService._() {
     _logStartup();
+    _attachErrorHandlers();
   }
 
   final List<LogEntry> _entries = [];
+
+  void _attachErrorHandlers() {
+    FlutterError.onError = (details) {
+      _add('ERROR', details.exceptionAsString(), details.stack?.toString());
+    };
+
+    PlatformDispatcher.instance.onError = (error, stack) {
+      _add('FATAL', error.toString(), stack.toString());
+      return true;
+    };
+  }
 
   Future<void> _logStartup() async {
     try {
